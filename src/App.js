49716eb4f816b766b9dc1ea2d843e72f84fd164b
Gabriel Rufino <gabriel.rufino@betrybe.com>
2021-01-05 15:27:19 -0300

    FIrst version ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import 'bulma/css/bulma.min.css'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './components/Header';
+import Form from './components/Form';
+import Result from './components/Result';
+
+class App extends React.Component {
+  render () {
+    return (
+      <div className="container">
+        <Header />
+
+        <div className="columns">
+          <div className="column">
+            <Form />
+          </div>
+          <div className="column">
+            <Result />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render () {
+    return (
+      <h1 className="title has-text-centered has-text-primary is-size-1">
+        What is my age?
+      </h1>
+    )
+  }
+}
+
+export default Header;
